3d26fe76bfbb1520c382c78d59ff68f0074e16e5
lukileczo <lukileczo@gmail.com>
2022-04-08 22:08:14 +0200

    server communication

--- RaspberryPi/rfid_test.py
+++ RaspberryPi/rfid_test.py
@@ -1,32 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
 from lcd import LCD
 from mfrc522 import SimpleMFRC522
+import requests
+from time import sleep
+import json
+
+PLACEID = 1
+POSTURI = "http://192.168.43.145:5000/"
 
 
-def writeData(reader):
-    try:
-        text = input("New data:")
-        print("Now place your tag to write")
-        reader.write(text)
-        print("Written")
-    finally:
-        GPIO.cleanup()
-
-
-def readData(reader):
-    try:
-        id, text = reader.read()
-        print(id)
-        print(text)
-    finally:
-        GPIO.cleanup()
+class Visitor:
+    def __init__(self, name, surname, points) -> None:
+        self.name = name
+        self.surname = surname
+        self.points = points
 
 
 if __name__ == "__main__":
     lcd = LCD()
     lcd.clear
     reader = SimpleMFRC522()
-    id, text = reader.read()
-    lcd.message = str(id)
+
+    while True:
+        lcd.message = "Przyloz karte"
+        rfid, text = reader.read()
+        personId = str(rfid)
+        lcd.clear()
+        lcd.message = str(rfid)
+        response = requests.post(
+            POSTURI + "visit", json={"place_id": PLACEID, "person_id": personId}
+        )
+        if response.status_code == 200:
+            print(response.content.decode())
+            respDict = json.loads(response.content.decode())
+            visitor = Visitor(**respDict)
+            lcd.cursor_position(0, 1)
+            lcd.message = f"Witaj {visitor.name}"
+
+        sleep(1)
