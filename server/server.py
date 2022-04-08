58e55eaf31e3bec9a00a6c411e53bb8344e5a4c8
KrystianMand <krystianmand245@gmail.com>
2022-04-08 21:10:03 +0200

    GET personID, POST visit, POST task

--- server/server.py
+++ server/server.py
@@ -1,14 +1,44 @@
-from flask import Flask
+from unicodedata import name
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
-state = {
-    'level': 50
-}
+class PersonId(Resource):
+    def get(self, id):
+        print(id)  
+        
+        return {"person": "id"}
+    
+class Visit(Resource):
+    def __init__(self):
+        self.args_parser = reqparse.RequestParser()
+        
+        self.args_parser.add_argument(name='person_id', required=True)
+        self.args_parser.add_argument(name='place_id', required=True)
+        
+    def post(self):
+        args = self.args_parser.parse_args()
+        
+        return "OK"   
 
-@app.route("/")
-def home():
-    return state
+class Task(Resource):
+    def __init__(self):
+        self.args_parser = reqparse.RequestParser()
+        
+        self.args_parser.add_argument(name='task_id', required=True)
+        self.args_parser.add_argument(name='person_id', required=True)
+        
+    def post(self):
+        args = self.args_parser.parse_args()
+        
+        return args.task_id
+        
+    
+api.add_resource(PersonId, '/person/<id>')
+api.add_resource(Visit, '/visit')
+api.add_resource(Task, '/task')
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
