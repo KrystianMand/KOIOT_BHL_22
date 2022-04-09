f099c6989ebd5b18cb73564d0977d91377b35010
KrystianMand <krystianmand245@gmail.com>
2022-04-09 02:12:55 +0200

    audsa

--- server/server.py
+++ server/server.py
@@ -9,6 +9,8 @@ from datetime import datetime
 app = Flask(__name__)
 api = Api(app)
 
+HOST = '192.168.43.145'
+
 # SQLAlchemy config. Read more: https://flask-sqlalchemy.palletsprojects.com/en/2.x/
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///persons.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -64,10 +66,9 @@ class Visit(Resource):
             # if Visits.query.filter_by(id=args.person_id):
             today = str(datetime.today().date())
             visits = Visits.query.filter_by(person_id=args.person_id, place_id=args.place_id).all()
-            print(visits[-1].date)
             # splitted_visit = visits[-1].date.split(' ')
             # day_of_visit = splitted_visit[0]
-            if visits:
+            if visits and len(visits) > 0:
                 if visits[-1].date == today: # Last element in the list (date of last visit)
                     return {"is_exist": True, "is_visited": True}
                 else:
@@ -112,4 +113,4 @@ api.add_resource(Visit, '/visit')
 api.add_resource(Task, '/task')
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host=HOST, debug=True)
